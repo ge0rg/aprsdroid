e3db643820076976f59a8c9ae3cc49b099f32f4a
Georg Lukas <georg@op-co.de>
2010-01-01 03:37:27 +0100

    implemented basic location req, need scala 2.8

--- src/de/duenndns/aprsdroid/APRSdroid.scala
+++ src/de/duenndns/aprsdroid/APRSdroid.scala
@@ -1,19 +1,61 @@
 package de.duenndns.aprsdroid
 
 import _root_.android.app.Activity
+import _root_.android.content.Context
+import _root_.android.location._
 import _root_.android.os.Bundle
+import _root_.android.util.Log
+import _root_.android.view.View
+import _root_.android.view.View.OnClickListener
+import _root_.android.widget.Button
+import _root_.android.widget.TextView
 
-class APRSdroid extends Activity {
+class APRSdroid extends Activity with LocationListener with OnClickListener {
+	val UPDATE_TIME = 10000 // 10k ms = 10s
+	val UPDATE_DIST = 10 // 10m
+	var locMan : LocationManager = null
+        var lat : TextView = null
+        var lon : TextView = null
+        var status : TextView = null
+
+	val TAG = "APRSdroid"
+
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
-			setContentView(R.layout.main)
+		setContentView(R.layout.main)
+		locMan = getSystemService(Context.LOCATION_SERVICE).asInstanceOf[LocationManager]
+		locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+			UPDATE_TIME, UPDATE_DIST, this)
+
+                lat = findViewById(R.id.lat).asInstanceOf[TextView]
+                lon = findViewById(R.id.lon).asInstanceOf[TextView]
+                status = findViewById(R.id.status).asInstanceOf[TextView]
+                findViewById(R.id.startbtn).asInstanceOf[Button].setOnClickListener(this);
+                findViewById(R.id.stopbtn).asInstanceOf[Button].setOnClickListener(this);
+                findViewById(R.id.singlebtn).asInstanceOf[Button].setOnClickListener(this);
 	}
-	override def $tag() : Int = {
-		try {
-			return super.$tag();
-		} catch {
-			case e: Exception => throw new RuntimeException(e);
-		}
+
+	override def onLocationChanged(location : Location) {
+		Log.d(TAG, "onLocationChanged: " + location)
+		lat.setText("lat: " + location.getLatitude)
+		lon.setText("lon: " + location.getLongitude)
 	}
+	override def onProviderDisabled(provider : String) {
+		Log.d(TAG, "onProviderDisabled: " + provider)
+		status.setText(provider + " disabled")
+	}
+	override def onProviderEnabled(provider : String) {
+		Log.d(TAG, "onProviderEnabled: " + provider)
+		status.setText(provider + " enabled")
+	}
+	override def onStatusChanged(provider : String, st: Int, extras : Bundle) {
+		Log.d(TAG, "onStatusChanged: " + provider)
+		status.setText("status: " + provider + "/" + st);
+	}
+	override def onClick(view : View) {
+		Log.d(TAG, "onClick: " + view + "/" + view.getId)
+                status.setText(view.asInstanceOf[Button].getText)
+	}
+
 }
 
