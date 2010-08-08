9b343bbfcb8f20f702f1230e8f2ee09cb2da4827
Georg Lukas <georg@op-co.de>
2010-08-08 19:22:19 +0200

    preliminary support for android.Proxy

--- src/HttpPostUploader.scala
+++ src/HttpPostUploader.scala
@@ -3,10 +3,12 @@ package de.duenndns.aprsdroid
 import _root_.android.content.SharedPreferences
 import _root_.android.location.Location
 import _root_.android.util.Log
+import _root_.android.net.Proxy
 import _root_.org.apache.http._
 import _root_.org.apache.http.entity.StringEntity
 import _root_.org.apache.http.impl.client.DefaultHttpClient
 import _root_.org.apache.http.client.methods.HttpPost
+import _root_.org.apache.http.conn.params.ConnRouteParams
 
 class HttpPostUploader(host : String, login : String) extends AprsIsUploader(host, login) {
 	val TAG = "AprsHttpPost"
@@ -17,6 +19,13 @@ class HttpPostUploader(host : String, login : String) extends AprsIsUploader(hos
 	def doPost(urlString : String, content : String) : String = {
 		val client = new DefaultHttpClient()
 		val post = new HttpPost(urlString)
+		val proxyHost = Proxy.getDefaultHost()
+		val proxyPort = Proxy.getDefaultPort()
+		if (proxyHost != null && proxyPort != -1) {
+			Log.i(TAG, "doPost(): Using proxy " + proxyHost + ":" + proxyPort)
+			ConnRouteParams.setDefaultProxy(post.getParams(),
+				new HttpHost(proxyHost, proxyPort, "http"))
+		}
 		post.setEntity(new StringEntity(content))
 		post.addHeader("Content-Type", "application/octet-stream");
 		post.addHeader("Accept-Type", "text/plain");
