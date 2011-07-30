faa91619c79fed70b0ceb9db4fb132e10aaa4709
Georg Lukas <georg@op-co.de>
2011-07-30 14:10:05 +0200

    outsource message handling to own sub-service

--- src/AprsService.scala
+++ src/AprsService.scala
@@ -1,7 +1,7 @@
 package org.aprsdroid.app
 
 import _root_.android.app.Service
-import _root_.android.content.{BroadcastReceiver, ContentValues, Context, Intent, IntentFilter}
+import _root_.android.content.{Context, Intent, IntentFilter}
 import _root_.android.location._
 import _root_.android.os.{Bundle, IBinder, Handler}
 import _root_.android.preference.PreferenceManager
@@ -24,8 +24,6 @@ object AprsService {
 	val PACKET = PACKAGE + ".PACKET"
 
 	val FAST_LANE_ACT = 30000
-
-	val NUM_OF_RETRIES = 7
 
 	def intent(ctx : Context, action : String) : Intent = {
 		new Intent(action, null, ctx, classOf[AprsService])
@@ -52,11 +50,8 @@ class AprsService extends Service with LocationListener {
 
 	lazy val db = StorageDatabase.open(this)
 
-	lazy val msgNotifier = new BroadcastReceiver() {
-		override def onReceive(ctx : Context, i : Intent) {
-			sendPendingMessages()
-		}
-	}
+	var msgService = new MessageService(this)
+	lazy val msgNotifier = msgService.createMessageNotifier()
 
 	var poster : AprsIsUploader = null
 
@@ -239,7 +234,7 @@ class AprsService extends Service with LocationListener {
 
 		Log.d(TAG, "packet: " + packet)
 		val result = try {
-			sendPendingMessages()
+			msgService.sendPendingMessages()
 			val status = poster.update(packet)
 			i.putExtra(STATUS, status)
 			i.putExtra(PACKET, packet.toString)
@@ -279,29 +274,6 @@ class AprsService extends Service with LocationListener {
 		Log.d(TAG, "onStatusChanged: " + provider)
 	}
 
-	def handleMessage(ts : Long, ap : APRSPacket, msg : MessagePacket) {
-		val callssid = prefs.getCallSsid()
-		if (msg.getTargetCallsign() == callssid) {
-			if (msg.isAck() || msg.isRej()) {
-				val new_type = if (msg.isAck())
-					StorageDatabase.Message.TYPE_OUT_ACKED
-				else
-					StorageDatabase.Message.TYPE_OUT_REJECTED
-				db.updateMessageAcked(ap.getSourceCall(), msg.getMessageNumber(), new_type)
-			} else {
-				db.addMessage(ts, ap.getSourceCall(), msg)
-				if (msg.getMessageNumber() != "") {
-					// we need to send an ack
-					val ack = AprsPacket.formatMessage(callssid, appVersion(), ap.getSourceCall(), "ack", msg.getMessageNumber())
-					val status = poster.update(ack)
-					addPost(StorageDatabase.Post.TYPE_POST, status, ack.toString)
-				}
-				ServiceNotifier.instance.notifyMessage(this, ap.getSourceCall(), msg.getMessageBody())
-			}
-			sendBroadcast(new Intent(AprsService.MESSAGE).putExtra(STATUS, ap.toString))
-		}
-	}
-
 	def parsePacket(ts : Long, message : String) {
 		try {
 			val fap = new Parser().parse(message)
@@ -314,7 +286,7 @@ class AprsService extends Service with LocationListener {
 			fap.getAprsInformation() match {
 				case pp : PositionPacket => db.addPosition(ts, fap, pp.getPosition(), null)
 				case op : ObjectPacket => db.addPosition(ts, fap, op.getPosition(), op.getObjectName())
-				case msg : MessagePacket => handleMessage(ts, fap, msg)
+				case msg : MessagePacket => msgService.handleMessage(ts, fap, msg)
 			}
 		} catch {
 		case e : Exception =>
@@ -338,7 +310,7 @@ class AprsService extends Service with LocationListener {
 	def postSubmit(post : String) {
 		handler.post {
 			addPost(StorageDatabase.Post.TYPE_INCMG, "incoming", post)
-			sendPendingMessages()
+			msgService.sendPendingMessages()
 		}
 	}
 
@@ -349,46 +321,5 @@ class AprsService extends Service with LocationListener {
 		}
 	}
 
-	def canSendMsg(ts : Long, retrycnt : Int) : Boolean = {
-		if (retrycnt == 0)
-			true
-		else {
-			//val delta = 30000*scala.math.pow(2, retrycnt-1).toLong
-			val delta = 30000 * (1 << (retrycnt - 1))
-			(ts + delta < System.currentTimeMillis)
-		}
-	}
-
-	def sendPendingMessages() {
-		import StorageDatabase.Message._
-
-		val callssid = prefs.getCallSsid()
-
-		val c = db.getPendingMessages(NUM_OF_RETRIES)
-		//Log.d(TAG, "sendPendingMessages")
-		c.moveToFirst()
-		while (!c.isAfterLast()) {
-			val ts = c.getLong(COLUMN_TS)
-			val retrycnt = c.getInt(COLUMN_RETRYCNT)
-			val call = c.getString(COLUMN_CALL)
-			val msgid = c.getString(COLUMN_MSGID)
-			val msgtype = c.getInt(COLUMN_TYPE)
-			val text = c.getString(COLUMN_TEXT)
-			Log.d(TAG, "pending message: %d/%d ->%s '%s'".format(retrycnt, NUM_OF_RETRIES, call, text))
-			if (retrycnt < NUM_OF_RETRIES && canSendMsg(ts, retrycnt)) {
-				val msg = AprsPacket.formatMessage(callssid, appVersion(), call, text, msgid)
-				val status = poster.update(msg)
-				addPost(StorageDatabase.Post.TYPE_POST, status, msg.toString)
-				val cv = new ContentValues()
-				cv.put(RETRYCNT, (retrycnt + 1).asInstanceOf[java.lang.Integer])
-				cv.put(TS, System.currentTimeMillis.asInstanceOf[java.lang.Long])
-				// XXX: do not ack until acked
-				db.updateMessage(c.getLong(/* COLUMN_ID */ 0), cv)
-				sendBroadcast(new Intent(AprsService.MESSAGE).putExtra(STATUS, msg.toString))
-			}
-			c.moveToNext()
-		}
-		c.close()
-	}
 }
 
---
+++ src/MessageService.scala
@@ -0,0 +1,84 @@
+package org.aprsdroid.app
+
+import _root_.android.content.{BroadcastReceiver, ContentValues, Context, Intent}
+import _root_.android.util.Log
+import _root_.android.os.Handler
+
+import _root_.net.ab0oo.aprs.parser._
+
+class MessageService(s : AprsService) {
+	val TAG = "APRSdroid.MsgService"
+
+	val NUM_OF_RETRIES = 7
+
+	def createMessageNotifier() = new BroadcastReceiver() {
+		override def onReceive(ctx : Context, i : Intent) {
+			sendPendingMessages()
+		}
+	}
+
+	def handleMessage(ts : Long, ap : APRSPacket, msg : MessagePacket) {
+		val callssid = s.prefs.getCallSsid()
+		if (msg.getTargetCallsign() == callssid) {
+			if (msg.isAck() || msg.isRej()) {
+				val new_type = if (msg.isAck())
+					StorageDatabase.Message.TYPE_OUT_ACKED
+				else
+					StorageDatabase.Message.TYPE_OUT_REJECTED
+				s.db.updateMessageAcked(ap.getSourceCall(), msg.getMessageNumber(), new_type)
+			} else {
+				s.db.addMessage(ts, ap.getSourceCall(), msg)
+				if (msg.getMessageNumber() != "") {
+					// we need to send an ack
+					val ack = AprsPacket.formatMessage(callssid, s.appVersion(), ap.getSourceCall(), "ack", msg.getMessageNumber())
+					val status = s.poster.update(ack)
+					s.addPost(StorageDatabase.Post.TYPE_POST, status, ack.toString)
+				}
+				ServiceNotifier.instance.notifyMessage(s, ap.getSourceCall(), msg.getMessageBody())
+			}
+			s.sendBroadcast(new Intent(AprsService.MESSAGE).putExtra(AprsService.STATUS, ap.toString))
+		}
+	}
+
+	def canSendMsg(ts : Long, retrycnt : Int) : Boolean = {
+		if (retrycnt == 0)
+			true
+		else {
+			//val delta = 30000*scala.math.pow(2, retrycnt-1).toLong
+			val delta = 30000 * (1 << (retrycnt - 1))
+			(ts + delta < System.currentTimeMillis)
+		}
+	}
+
+	def sendPendingMessages() {
+		import StorageDatabase.Message._
+
+		val callssid = s.prefs.getCallSsid()
+
+		val c = s.db.getPendingMessages(NUM_OF_RETRIES)
+		//Log.d(TAG, "sendPendingMessages")
+		c.moveToFirst()
+		while (!c.isAfterLast()) {
+			val ts = c.getLong(COLUMN_TS)
+			val retrycnt = c.getInt(COLUMN_RETRYCNT)
+			val call = c.getString(COLUMN_CALL)
+			val msgid = c.getString(COLUMN_MSGID)
+			val msgtype = c.getInt(COLUMN_TYPE)
+			val text = c.getString(COLUMN_TEXT)
+			Log.d(TAG, "pending message: %d/%d ->%s '%s'".format(retrycnt, NUM_OF_RETRIES, call, text))
+			if (retrycnt < NUM_OF_RETRIES && canSendMsg(ts, retrycnt)) {
+				val msg = AprsPacket.formatMessage(callssid, s.appVersion(), call, text, msgid)
+				val status = s.poster.update(msg)
+				s.addPost(StorageDatabase.Post.TYPE_POST, status, msg.toString)
+				val cv = new ContentValues()
+				cv.put(RETRYCNT, (retrycnt + 1).asInstanceOf[java.lang.Integer])
+				cv.put(TS, System.currentTimeMillis.asInstanceOf[java.lang.Long])
+				// XXX: do not ack until acked
+				s.db.updateMessage(c.getLong(/* COLUMN_ID */ 0), cv)
+				s.sendBroadcast(new Intent(AprsService.MESSAGE).putExtra(AprsService.STATUS, msg.toString))
+			}
+			c.moveToNext()
+		}
+		c.close()
+	}
+}
