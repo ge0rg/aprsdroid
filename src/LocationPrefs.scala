25d1a3be13927659dbfb6713dbdf847fd65d4ae9
Georg Lukas <georg@op-co.de>
2011-11-11 00:49:28 +0100

    add manual position from GPS

--- src/LocationPrefs.scala
+++ src/LocationPrefs.scala
@@ -1,5 +1,7 @@
 package org.aprsdroid.app
 
+import _root_.android.content.Context
+import _root_.android.location.{Location, LocationManager}
 import _root_.android.os.Bundle
 import _root_.android.preference.{PreferenceActivity, PreferenceManager}
 
@@ -7,6 +9,26 @@ class LocationPrefs extends PreferenceActivity {
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		val prefs = new PrefsWrapper(this)
+		if (checkGpsManualPosition(prefs)) {
+			finish()
+			return
+		}
 		addPreferencesFromResource(LocationSource.instanciatePrefsAct(prefs))
+	}
+
+	def checkGpsManualPosition(prefs : PrefsWrapper) = {
+		val i = getIntent()
+		if (i != null && i.getDataString() != null && i.getDataString().equals("gps2manual")) {
+			val l = getSystemService(Context.LOCATION_SERVICE).asInstanceOf[LocationManager]
+					.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+			if (l != null) {
+				val pe = prefs.prefs.edit()
+				pe.putString("manual_lat", l.getLatitude().toString())
+				pe.putString("manual_lon", l.getLongitude().toString())
+				pe.commit()
+			}
+			true
+		}
+		false
 	}
 }
