ec9df9c1cca413d9b5be7d4eccfb36439a307f15
Georg Lukas <georg@op-co.de>
2012-03-21 18:33:50 +0100

    manual position: fix getting from GPS

--- src/LocationPrefs.scala
+++ src/LocationPrefs.scala
@@ -6,29 +6,25 @@ import _root_.android.os.Bundle
 import _root_.android.preference.{PreferenceActivity, PreferenceManager}
 
 class LocationPrefs extends PreferenceActivity {
+
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		val prefs = new PrefsWrapper(this)
-		if (checkGpsManualPosition(prefs)) {
-			finish()
-			return
-		}
 		addPreferencesFromResource(LocationSource.instanciatePrefsAct(prefs))
 	}
 
-	def checkGpsManualPosition(prefs : PrefsWrapper) = {
-		val i = getIntent()
+	override def onNewIntent(i : android.content.Intent) {
 		if (i != null && i.getDataString() != null && i.getDataString().equals("gps2manual")) {
 			val l = getSystemService(Context.LOCATION_SERVICE).asInstanceOf[LocationManager]
 					.getLastKnownLocation(LocationManager.GPS_PROVIDER)
 			if (l != null) {
-				val pe = prefs.prefs.edit()
+				val pe = new PrefsWrapper(this).prefs.edit()
 				pe.putString("manual_lat", l.getLatitude().toString())
 				pe.putString("manual_lon", l.getLongitude().toString())
 				pe.commit()
 			}
-			true
+			finish()
+			startActivity(i)
 		}
-		false
 	}
 }
