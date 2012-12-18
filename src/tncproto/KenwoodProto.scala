e29a1a32065544b7e92faefeccf17e8496a40aea
Georg Lukas <georg@op-co.de>
2012-12-18 12:57:20 +0100

    kenwood: Object names are filled with whitespace, workaround

--- src/tncproto/KenwoodProto.scala
+++ src/tncproto/KenwoodProto.scala
@@ -15,13 +15,13 @@ class KenwoodProto(is : InputStream) extends TncProto(is, null) {
 		case "$PKWDWPL" =>
 			val lat = "%s%s".format(s(3), s(4))
 			val lon = "%s%s".format(s(5), s(6))
-			val call = s(11)
+			val call = s(11).trim()
 			val sym = s(12)
 			"%s>APRS:!%s%s%s%s".format(call, lat, sym(0), lon, sym(1))
 		case "$GPWPL" =>
 			val lat = "%s%s".format(s(1), s(2))
 			val lon = "%s%s".format(s(3), s(4))
-			val call = s(5)
+			val call = s(5).trim()
 			"%s>APRS:!%s/%s/".format(call, lat, lon)
 		case _ => line
 		}
