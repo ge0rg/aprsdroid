dfa19bf219739f8ea6f294867eeccf789b8735ac
Georg Lukas <georg@op-co.de>
2014-09-14 15:30:20 +0200

    OSM: refactor file chooser

--- src/PrefsAct.scala
+++ src/PrefsAct.scala
@@ -2,21 +2,26 @@ package org.aprsdroid.app
 
 import _root_.android.content.Intent
 import _root_.android.os.Bundle
+import _root_.android.preference.Preference
+import _root_.android.preference.Preference.OnPreferenceClickListener
 import _root_.android.preference.PreferenceActivity
 import _root_.android.preference.PreferenceManager
 
 class PrefsAct extends PreferenceActivity {
+	def fileChooserPreference(pref_name : String, reqCode : Int) {
+		findPreference(pref_name).setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			def onPreferenceClick(preference : Preference) = {
+				val get_file = new Intent(Intent.ACTION_GET_CONTENT).setType("*/*")
+				startActivityForResult(Intent.createChooser(get_file,
+					getString(R.string.p_mapfile_choose)), reqCode)
+				true
+			}
+		});
+	}
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		addPreferencesFromResource(R.xml.preferences)
-	}
-
-	override def startActivity(i : Intent) {
-		if (Intent.ACTION_GET_CONTENT == i.getAction())
-			startActivityForResult(Intent.createChooser(i, 
-				getString(R.string.p_mapfile_choose)), 123456)
-		else
-			super.startActivity(i)
+		fileChooserPreference("mapfile", 123456)
 	}
 
 	override def onActivityResult(reqCode : Int, resultCode : Int, data : Intent) {
