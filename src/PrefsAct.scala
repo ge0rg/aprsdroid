c2a4e65b95552351fbb39cdf553e838e3f489345
Georg Lukas <georg@op-co.de>
2018-05-25 19:22:06 +0200

    Prefs: refactor title id

--- src/PrefsAct.scala
+++ src/PrefsAct.scala
@@ -10,12 +10,12 @@ import _root_.android.preference.PreferenceActivity
 import _root_.android.preference.PreferenceManager
 
 class PrefsAct extends PreferenceActivity {
-	def fileChooserPreference(pref_name : String, reqCode : Int) {
+	def fileChooserPreference(pref_name : String, reqCode : Int, titleId : Int) {
 		findPreference(pref_name).setOnPreferenceClickListener(new OnPreferenceClickListener() {
 			def onPreferenceClick(preference : Preference) = {
 				val get_file = new Intent(Intent.ACTION_GET_CONTENT).setType("*/*")
 				startActivityForResult(Intent.createChooser(get_file,
-					getString(R.string.p_mapfile_choose)), reqCode)
+					getString(titleId)), reqCode)
 				true
 			}
 		});
@@ -23,8 +23,8 @@ class PrefsAct extends PreferenceActivity {
 	override def onCreate(savedInstanceState: Bundle) {
 		super.onCreate(savedInstanceState)
 		addPreferencesFromResource(R.xml.preferences)
-		fileChooserPreference("mapfile", 123456)
-		fileChooserPreference("themefile", 123457)
+		fileChooserPreference("mapfile", 123456, R.string.p_mapfile_choose)
+		fileChooserPreference("themefile", 123457, R.string.p_themefile_choose)
 	}
 
 	def resolveContentUri(uri : Uri) = {
