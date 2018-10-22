88408aedf3241bd5d528b1a344daf50307f0d73e
Georg Lukas <georg@op-co.de>
2018-10-22 13:39:41 +0200

    Prefs: auto-uppercase callsign input; fix #184 and #190

--- src/EditTextPreferenceWithValue.java
+++ src/EditTextPreferenceWithValue.java
@@ -2,8 +2,11 @@ package de.duenndns;
 
 import android.content.Context;
 import android.preference.EditTextPreference;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditTextPreferenceWithValue extends EditTextPreference {
 	CharSequence mSummary;
@@ -14,6 +17,23 @@ public class EditTextPreferenceWithValue extends EditTextPreference {
 
 	public EditTextPreferenceWithValue(Context context) {
 		super(context);
+	}
+
+	private void fixupCaps() {
+		EditText et = getEditText();
+		if ((et.getInputType() & InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS) != 0) {
+			// append AllCaps filter
+			InputFilter[] filters = et.getFilters();
+			InputFilter[] newFilters = new InputFilter[filters.length + 1];
+			System.arraycopy(filters, 0, newFilters, 0, filters.length);
+			newFilters[filters.length] = new InputFilter.AllCaps();
+			et.setFilters(newFilters);
+		}
+	}
+	@Override
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		fixupCaps();
 	}
 
 	private void setSummaryToText(String text) {
